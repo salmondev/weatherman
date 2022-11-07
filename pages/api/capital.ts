3b96a0b5bf4556d42a6a2d82f754a34efe7c9ea4
ARMY <panurut.c@merudy.com>
2022-11-08 05:13:33 +0700

    update : autocomplete and timezone

---
+++ pages/api/capital.ts
@@ -0,0 +1,751 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Capital = {
+  name: string;
+};
+
+// const Capital = [
+//   { name: "New York" },
+//   { name: "Tokyo" },
+//   { name: "Bangkok" },
+//   { name: "London" },
+//   { name: "Sydney" },
+// ];
+
+const Capital = [
+  {
+    name: "Abu Dhabi",
+  },
+  {
+    name: "Abuja",
+  },
+  {
+    name: "Accra",
+  },
+  {
+    name: "Adamstown",
+  },
+  {
+    name: "Addis Ababa",
+  },
+  {
+    name: "Algiers",
+  },
+  {
+    name: "Alofi",
+  },
+  {
+    name: "Amman",
+  },
+  {
+    name: "Amsterdam",
+  },
+  {
+    name: "Andorra la Vella",
+  },
+  {
+    name: "Ankara",
+  },
+  {
+    name: "Antananarivo",
+  },
+  {
+    name: "Apia",
+  },
+  {
+    name: "Ashgabat",
+  },
+  {
+    name: "Asmara",
+  },
+  {
+    name: "Astana",
+  },
+  {
+    name: "Asuncion",
+  },
+  {
+    name: "Athens",
+  },
+  {
+    name: "Avarua",
+  },
+  {
+    name: "Baghdad",
+  },
+  {
+    name: "Baku",
+  },
+  {
+    name: "Bamako",
+  },
+  {
+    name: "Bandar Seri Begawan",
+  },
+  {
+    name: "Bangkok",
+  },
+  {
+    name: "Bangui",
+  },
+  {
+    name: "Banjul",
+  },
+  {
+    name: "Basse-Terre",
+  },
+  {
+    name: "Basseterre",
+  },
+  {
+    name: "Beijing",
+  },
+  {
+    name: "Beirut",
+  },
+  {
+    name: "Belgrade",
+  },
+  {
+    name: "Belmopan",
+  },
+  {
+    name: "Berlin",
+  },
+  {
+    name: "Berne",
+  },
+  {
+    name: "Bishkek",
+  },
+  {
+    name: "Bissau",
+  },
+  {
+    name: "Bogota",
+  },
+  {
+    name: "Brasilia",
+  },
+  {
+    name: "Bratislava",
+  },
+  {
+    name: "Brazzaville",
+  },
+  {
+    name: "Bridgetown",
+  },
+  {
+    name: "Brussels",
+  },
+  {
+    name: "Bucharest",
+  },
+  {
+    name: "Budapest",
+  },
+  {
+    name: "Buenos Aires",
+  },
+  {
+    name: "Bujumbura",
+  },
+  {
+    name: "Cairo",
+  },
+  {
+    name: "Canberra",
+  },
+  {
+    name: "Caracas",
+  },
+  {
+    name: "Castries",
+  },
+  {
+    name: "Charlotte Amalie",
+  },
+  {
+    name: "Chisinau",
+  },
+  {
+    name: "Cockburn Town",
+  },
+  {
+    name: "Colombo",
+  },
+  {
+    name: "Conakry",
+  },
+  {
+    name: "Copenhagen",
+  },
+  {
+    name: "Dakar",
+  },
+  {
+    name: "Damascus",
+  },
+  {
+    name: "Dhaka",
+  },
+  {
+    name: "Diego Garcia",
+  },
+  {
+    name: "Dili",
+  },
+  {
+    name: "Djibouti",
+  },
+  {
+    name: "Dodoma",
+  },
+  {
+    name: "Doha",
+  },
+  {
+    name: "Douglas, Isle of Man",
+  },
+  {
+    name: "Dublin",
+  },
+  {
+    name: "Dushanbe",
+  },
+  {
+    name: "East Jerusalem",
+  },
+  {
+    name: "El-Aaiun",
+  },
+  {
+    name: "Flying Fish Cove",
+  },
+  {
+    name: "Fort-de-France",
+  },
+  {
+    name: "Freetown",
+  },
+  {
+    name: "Funafuti",
+  },
+  {
+    name: "Gaborone",
+  },
+  {
+    name: "George Town",
+  },
+  {
+    name: "Georgetown",
+  },
+  {
+    name: "Gibraltar",
+  },
+  {
+    name: "Grytviken",
+  },
+  {
+    name: "Guatemala City",
+  },
+  {
+    name: "Gustavia",
+  },
+  {
+    name: "Hagatna",
+  },
+  {
+    name: "Hamilton",
+  },
+  {
+    name: "Hanoi",
+  },
+  {
+    name: "Harare",
+  },
+  {
+    name: "Havana",
+  },
+  {
+    name: "Helsinki",
+  },
+  {
+    name: "Hong Kong",
+  },
+  {
+    name: "Honiara",
+  },
+  {
+    name: "Islamabad",
+  },
+  {
+    name: "Jakarta",
+  },
+  {
+    name: "Jamestown",
+  },
+  {
+    name: "Jerusalem",
+  },
+  {
+    name: "Juba",
+  },
+  {
+    name: "Kabul",
+  },
+  {
+    name: "Kampala",
+  },
+  {
+    name: "Kathmandu",
+  },
+  {
+    name: "Khartoum",
+  },
+  {
+    name: "Kiev",
+  },
+  {
+    name: "Kigali",
+  },
+  {
+    name: "Kingston",
+  },
+  {
+    name: "Kingstown",
+  },
+  {
+    name: "Kinshasa",
+  },
+  {
+    name: "Kuala Lumpur",
+  },
+  {
+    name: "Kuwait City",
+  },
+  {
+    name: "Libreville",
+  },
+  {
+    name: "Lilongwe",
+  },
+  {
+    name: "Lima",
+  },
+  {
+    name: "Lisbon",
+  },
+  {
+    name: "Ljubljana",
+  },
+  {
+    name: "Lome",
+  },
+  {
+    name: "London",
+  },
+  {
+    name: "Longyearbyen",
+  },
+  {
+    name: "Luanda",
+  },
+  {
+    name: "Lusaka",
+  },
+  {
+    name: "Luxembourg",
+  },
+  {
+    name: "Macao",
+  },
+  {
+    name: "Madrid",
+  },
+  {
+    name: "Majuro",
+  },
+  {
+    name: "Malabo",
+  },
+  {
+    name: "Male",
+  },
+  {
+    name: "Mamoudzou",
+  },
+  {
+    name: "Managua",
+  },
+  {
+    name: "Manama",
+  },
+  {
+    name: "Manila",
+  },
+  {
+    name: "Maputo",
+  },
+  {
+    name: "Mariehamn",
+  },
+  {
+    name: "Marigot",
+  },
+  {
+    name: "Maseru",
+  },
+  {
+    name: "Mata Utu",
+  },
+  {
+    name: "Mbabane",
+  },
+  {
+    name: "Melekeok",
+  },
+  {
+    name: "Mexico City",
+  },
+  {
+    name: "Minsk",
+  },
+  {
+    name: "Mogadishu",
+  },
+  {
+    name: "Monaco",
+  },
+  {
+    name: "Monrovia",
+  },
+  {
+    name: "Montevideo",
+  },
+  {
+    name: "Moroni",
+  },
+  {
+    name: "Moscow",
+  },
+  {
+    name: "Muscat",
+  },
+  {
+    name: "N'Djamena",
+  },
+  {
+    name: "Nairobi",
+  },
+  {
+    name: "Nassau",
+  },
+  {
+    name: "Nay Pyi Taw",
+  },
+  {
+    name: "New Delhi",
+  },
+  {
+    name: "Niamey",
+  },
+  {
+    name: "Nicosia",
+  },
+  {
+    name: "Nouakchott",
+  },
+  {
+    name: "Noumea",
+  },
+  {
+    name: "Nuku'alofa",
+  },
+  {
+    name: "Nuuk",
+  },
+  {
+    name: "Oranjestad",
+  },
+  {
+    name: "Oslo",
+  },
+  {
+    name: "Ottawa",
+  },
+  {
+    name: "Ouagadougou",
+  },
+  {
+    name: "Pago Pago",
+  },
+  {
+    name: "Palikir",
+  },
+  {
+    name: "Panama City",
+  },
+  {
+    name: "Papeete",
+  },
+  {
+    name: "Paramaribo",
+  },
+  {
+    name: "Paris",
+  },
+  {
+    name: "Philipsburg",
+  },
+  {
+    name: "Phnom Penh",
+  },
+  {
+    name: "Plymouth",
+  },
+  {
+    name: "Podgorica",
+  },
+  {
+    name: "Port Louis",
+  },
+  {
+    name: "Port Moresby",
+  },
+  {
+    name: "Port Vila",
+  },
+  {
+    name: "Port of Spain",
+  },
+  {
+    name: "Port-au-Prince",
+  },
+  {
+    name: "Port-aux-Francais",
+  },
+  {
+    name: "Porto-Novo",
+  },
+  {
+    name: "Prague",
+  },
+  {
+    name: "Praia",
+  },
+  {
+    name: "Pretoria",
+  },
+  {
+    name: "Pristina",
+  },
+  {
+    name: "Pyongyang",
+  },
+  {
+    name: "Quito",
+  },
+  {
+    name: "Rabat",
+  },
+  {
+    name: "Reykjavik",
+  },
+  {
+    name: "Riga",
+  },
+  {
+    name: "Riyadh",
+  },
+  {
+    name: "Road Town",
+  },
+  {
+    name: "Rome",
+  },
+  {
+    name: "Roseau",
+  },
+  {
+    name: "Saint Helier",
+  },
+  {
+    name: "Saint-Denis",
+  },
+  {
+    name: "Saint-Pierre",
+  },
+  {
+    name: "Saipan",
+  },
+  {
+    name: "San Jose",
+  },
+  {
+    name: "San Juan",
+  },
+  {
+    name: "San Marino",
+  },
+  {
+    name: "San Salvador",
+  },
+  {
+    name: "Sanaa",
+  },
+  {
+    name: "Santiago",
+  },
+  {
+    name: "Santo Domingo",
+  },
+  {
+    name: "Sao Tome",
+  },
+  {
+    name: "Sarajevo",
+  },
+  {
+    name: "Seoul",
+  },
+  {
+    name: "Singapur",
+  },
+  {
+    name: "Skopje",
+  },
+  {
+    name: "Sofia",
+  },
+  {
+    name: "St Peter Port",
+  },
+  {
+    name: "St. George's",
+  },
+  {
+    name: "St. John's",
+  },
+  {
+    name: "Stanley",
+  },
+  {
+    name: "Stockholm",
+  },
+  {
+    name: "Sucre",
+  },
+  {
+    name: "Suva",
+  },
+  {
+    name: "Taipei",
+  },
+  {
+    name: "Tallinn",
+  },
+  {
+    name: "Tarawa",
+  },
+  {
+    name: "Tashkent",
+  },
+  {
+    name: "Tbilisi",
+  },
+  {
+    name: "Tegucigalpa",
+  },
+  {
+    name: "Tehran",
+  },
+  {
+    name: "The Valley",
+  },
+  {
+    name: "Thimphu",
+  },
+  {
+    name: "Tirana",
+  },
+  {
+    name: "Tokyo",
+  },
+  {
+    name: "Torshavn",
+  },
+  {
+    name: "Tripolis",
+  },
+  {
+    name: "Tunis",
+  },
+  {
+    name: "Ulan Bator",
+  },
+  {
+    name: "Vaduz",
+  },
+  {
+    name: "Valletta",
+  },
+  {
+    name: "Vatican City",
+  },
+  {
+    name: "Victoria",
+  },
+  {
+    name: "Vienna",
+  },
+  {
+    name: "Vientiane",
+  },
+  {
+    name: "Vilnius",
+  },
+  {
+    name: "Warsaw",
+  },
+  {
+    name: "Washington",
+  },
+  {
+    name: "Wellington",
+  },
+  {
+    name: "West Island",
+  },
+  {
+    name: "Willemstad",
+  },
+  {
+    name: "Windhoek",
+  },
+  {
+    name: "Yamoussoukro",
+  },
+  {
+    name: "Yaounde",
+  },
+  {
+    name: "Yaren",
+  },
+  {
+    name: "Yerevan",
+  },
+  {
+    name: "Zagreb",
+  },
+];
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Capital>,
+  next: NextFunction
+) {
+  res.status(200).json(Capital);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,31 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { Paper, Group, Text, TextInput, Button } from '@mantine/core'
-import { useState } from 'react'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { Paper, Group, Text, TextInput, Button } from "@mantine/core";
+import { useState, useEffect } from "react";
+import { Autocomplete, TextField } from "@mui/material";
+import PropTypes from "prop-types";
 
 const API_KEY = "427c653c4c7a71b95feebc57f8d32bd5";
 
 export default function Home() {
-  const [ cityInput, setCityInput ] = useState("");
+  const [value, setValue] = useState<string | null>(null);
 
-  const [weatherData, setWeatherData] = useState<any>({}); { city: "bangkok" }
+  const [cityInput, setCityInput] = useState<City | null>(null);
+  console.log({ cityInput });
+
+  const [weatherData, setWeatherData] = useState<any>({});
 
   async function getWeatherData() {
-    console.log("Button pressed");
     try {
       const serverResponse = await fetch(
         "https://api.openweathermap.org/data/2.5/weather?" +
-        "q=" +
-        cityInput +
-        "&appid=" +
-        API_KEY +
-        "&units=metric"
-      )
+          "q=" +
+          cityInput +
+          "&appid=" +
+          API_KEY +
+          "&units=metric"
+      );
       const data = await serverResponse.json();
       console.log(data);
       if (data?.cod === "400") throw data;
@@ -31,14 +35,92 @@ export default function Home() {
     }
   }
 
-  console.log(cityInput)
+  const [capitalData, setData] = useState<any>([]);
+
+  const capAPI = async () => {
+    try {
+      const res = await fetch(`http://localhost:3000/api/capital`);
+      const capitalData = await res.json();
+      console.log(capitalData);
+      const result = Object.values(capitalData);
+      setData(result);
+    } catch (err) {
+      console.log(error);
+    }
+  };
+
+  type City = {
+    name: string;
+  };
+
+  const citysOptions = capitalData.map((city) => city.name);
+
+  useEffect(() => {
+    setTimeout(() => {
+      capAPI();
+    }, 100);
+  }, []);
+
+  const dateBuilder = (timezone: number) => {
+    const mL = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    const dL = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    const date = new Date();
+    var day = date.getDay();
+    const dateN = date.getDate();
+    var month = date.getMonth();
+    const year = date.getFullYear();
+    day = dL[day];
+    month = mL[month];
+
+    var hour = date.getUTCHours();
+    var min = date.getMinutes();
+
+    const timezoneUTC = timezone / 3600;
+
+    hour = hour + timezoneUTC;
+    if (hour >= 24) {
+      hour = hour - 24;
+    }
+    hour = ("0" + hour).slice(-2);
+    min = ("0" + min).slice(-2);
+    const time = `${hour}:${min}`;
+
+    const today = `${day}  ${dateN}  ${month}  ${year}`;
+    const fullDate = today + " " + time;
+    return fullDate;
+  }
+
   return (
     <div
       style={{
         position: "static",
         height: "100vh",
-        backgroundImage: "url('https://littlevisuals.co/images/atlantic_ridge.jpg')",
-        backgroundSize: "cover"
+        backgroundImage:
+          "url('https://littlevisuals.co/images/atlantic_ridge.jpg')",
+        backgroundSize: "cover",
       }}
     >
       <div
@@ -46,56 +128,76 @@ export default function Home() {
           position: "absolute",
           left: "50%",
           top: "50%",
-          transform: "translate(-50%, -50%)"
+          transform: "translate(-50%, -50%)",
         }}
       >
-
-        <Paper withBorder p="lg" style={{maxWidth: "500px"}}>
-          <Group position="apart">
-            <Text size="xl" weight={500}>
-              Get The Weather!
-            </Text>
-          </Group>
-          <Group position="apart">
-            <Text size="lg">
-              Enter a city, and get the weather below!
-            </Text>
-          </Group>
-          <Group position="apart" mb="xs">
-            <TextInput
-              label="City Name"
-              placeholder="ex: Bangkok"
-              onChange={(e) => setCityInput(e.target.value)}
-            />
-          </Group>
-          <Group position="apart">
-            <Button variant='gradient' size='md' onClick={() => getWeatherData()}>
-              Get Weather
-            </Button>
-          </Group>
-          {Object.keys(weatherData).length !== 0 ?
+        <Paper withBorder p="lg" style={{ maxWidth: "1000px" }}>
+          {Object.keys(weatherData).length !== 0 ? (
             <>
               <Group position="left">
                 <Text>
-                  {weatherData.name} Weather
+                  <b>Weather</b> {weatherData.name}
+                  <br />
+                  <b>Date and Time</b> {dateBuilder(weatherData.timezone)}
+                  <br />
+                  <b>Status</b> {weatherData.weather[0].description}
                 </Text>
               </Group>
               <Group position="left">
                 <Image
-                  src={"https://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@4x.png"}
-                  width='100'
-                  height='100'
-                  alt='icon'
+                  src={
+                    "https://openweathermap.org/img/wn/" +
+                    weatherData.weather[0].icon +
+                    "@4x.png"
+                  }
+                  width="100"
+                  height="100"
+                  alt="icon"
                 />
                 <Text size="lg" weight={500}>
                   Currently {weatherData.main.temp} &deg;C
                 </Text>
               </Group>
             </>
-            : null
-          }
+          ) : null}
+          <Group position="apart">
+            <Text size="xl" weight={500}>
+              Get The Weather!
+            </Text>
+          </Group>
+          <Group position="apart">
+            <Text size="lg">Enter a city, and get the weather below!</Text>
+          </Group>
+          <Group position="apart" mb="xs">
+            <Autocomplete
+              options={citysOptions}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="City"
+                  onChange={(e) => setCityInput(e.target.value)}
+                />
+              )}
+              value={cityInput}
+              sx={{ width: 300 }}
+              onChange={(event: any, newValue: City | null) =>
+                setCityInput(newValue)
+              }
+              freeSolo
+            />
+          </Group>
+          <Group position="apart">
+            <Button
+              disabled={!cityInput}
+              constiant="gradient"
+              size="md"
+              onClick={() => getWeatherData()}
+            >
+              Get Weather
+            </Button>
+          </Group>
         </Paper>
       </div>
     </div>
-  )
+  );
 }
